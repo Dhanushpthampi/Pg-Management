600a92d18581d0e704c33a4091b0873717b001df
dhanushpthampi <dhanushpthampi@gmail.com>
2025-12-22 16:54:49 +0530

    changed color of sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -2,17 +2,16 @@ import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div style={{ width: "220px", background: "#111", color: "#fff", padding: "20px" }}>
+    <div style={{ width: "220px",borderRight: "1px solid #ddd", padding: "20px" }}>
       <h2>PG Admin</h2>
-
       <nav style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
-        <NavLink to="/" style={{ color: "#fff" }}>Dashboard</NavLink>
-        <NavLink to="/properties" style={{ color: "#fff" }}>Properties</NavLink>
-        <NavLink to="/tenants" style={{ color: "#fff" }}>Tenants</NavLink>
-        <NavLink to="/bookings" style={{ color: "#fff" }}>Bookings</NavLink>
-        <NavLink to="/staff" style={{ color: "#fff" }}>Staff</NavLink>
-        <NavLink to="/complaints" style={{ color: "#fff" }}>Complaints</NavLink>
-        <NavLink to="/invoices" style={{ color: "#fff" }}>Invoices</NavLink>
+        <NavLink to="/"  >Dashboard</NavLink>
+        <NavLink to="/properties"  >Properties</NavLink>
+        <NavLink to="/tenants"  >Tenants</NavLink>
+        <NavLink to="/bookings" >Bookings</NavLink>
+        <NavLink to="/staff" >Staff</NavLink>
+        <NavLink to="/complaints" >Complaints</NavLink>
+        <NavLink to="/invoices" >Invoices</NavLink>
       </nav>
     </div>
   );
